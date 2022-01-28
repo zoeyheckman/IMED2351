4c85485da5a21690998cdaa75cbec8c39b0682d4
zoeyheckman <43184084+zoeyheckman@users.noreply.github.com>
2022-01-27 19:36:03 -0600

    2.4 Assignment
    variables

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-console.log("How many chicken eggs do you think you could hold in one hand, at once?");
+var life = 100;
+var x = 1000;       //GLOBAL
+    
+{                     //BLOCK
+    let x, y, z;
+    x = 10;       
+    y = 20;         
+    z = x + y;       //mathematical
+}
 
+const food = {meat:"none", starch:"rice", broth:"vegetable"}; //CONST
+food.meat = "chicken";
 
+var name ='Dev Zoey'; //STRING
+
+console.log("name");
