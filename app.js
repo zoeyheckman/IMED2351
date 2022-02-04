2e938a1d640ab1d0b781984b05d009fbb5577a75
zoeyheckman <43184084+zoeyheckman@users.noreply.github.com>
2022-02-04 14:41:05 -0600

    Functions

--- app.js
+++ app.js
@@ -8,9 +8,32 @@ var x = 1000;       //GLOBAL
     z = x + y;       //mathematical
 }
 
-const food = {meat:"none", starch:"rice", broth:"vegetable"}; //CONST
-food.meat = "chicken";
-
-var name ='Dev Zoey'; //STRING
+const food = {          //Object with properties
+meat = "chicken",
+broth:"vegetable",
+starch:"rice",
+fullTitle : function() {
+    return this.meat + "and" + this.starch  
+}
+};
+const name = "Dev Zoey;"
 
 console.log("name");
+
+function toLower(text){                  //function 1
+   const lowerCased = text.toLowerCase();
+    console.log(lowerCased);
+}
+toLower(name);
+
+
+function adder(number1, number2){        //function 2
+    console.log(number1+number2)
+}
+
+adder(23,65);
+
+{
+let string ="abcdefghijklmnopqrstuvwxyz";   //STRING SLICE
+document.getElementById("demo").innerHTML =string.slice(2,11);
+}
