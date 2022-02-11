f63e41a358fa0168ac1d79a288be7f716d1402c6
zoeyheckman <43184084+zoeyheckman@users.noreply.github.com>
2022-02-11 16:46:14 -0600

    4.5 Assignment

--- app.js
+++ app.js
@@ -33,7 +33,57 @@ function adder(number1, number2){        //function 2
 
 adder(23,65);
 
+const name = "Zoey";        //CONCATENATED STRING VARIABLE
+const age = 23;
+const hobby1 = "baking";
+const hobby2 = "watching cartoons";
+console.log(`Hello, it's me, ${name}, and I am ${age} years old. I enjoy ${hobby1} and also ${hobby2}.`);
+
+if (time < 12){                       //CONDITIONAL STATEMENT
+    greeting = "Top of the morning to you!";
+} else if (time < 18){
+    greeting = "Top of the day to you!";
+} else {
+    greeting = "Top of the evening to you!";
+}
+
+switch (new Date().getDay()) {    //SWITCH STATEMENT
+    case 0:
+        day = "Monday";
+        text = "How was you're weekend?";
+        break;
+    case 1:
+        day = "Tuesday";
+        text = "How was you're weekend?";
+        break;
+    case 2:
+        day = "Wednesday";
+        text = "Halfway to the weekend. Woohoo!";
+        break;
+    case 3:
+        day = "Thursday";
+        text = "Almost weekend time.";
+        break;
+    case 4:
+        day = "Friday";
+        text = "Getting ready for the weekend?";
+        break;
+    case 5:
+        day = "Saturday";
+        text = "Having a nice weekend?";
+        break;
+    case 6:
+        day = "Sunday";
+        text = "Running out of weekend time.";
+        break;
+}
+
 {
 let string ="abcdefghijklmnopqrstuvwxyz";   //STRING SLICE
 document.getElementById("demo").innerHTML =string.slice(2,11);
+}
+
+{
+let x=new Date("1998-05-20");           //NUMBER METHOD
+document.getElementById("demo").innerHTML = Number(x);
 }
