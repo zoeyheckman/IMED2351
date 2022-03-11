d1d490ccb88dadf613e2d0de1dd1aa1c68359d14
Zoey Heckman <43184084+zoeyheckman@users.noreply.github.com>
2022-03-11 17:54:53 -0600

    7 Assignment

--- app.js
+++ app.js
@@ -1,3 +1,102 @@
+// 7 Assignment
+const game =()=>{
+    let pScore = 0;
+    let cScore =0;
+
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('match');
+
+        playBtn.addEventListener("click", () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
+    const playMatch = () => {
+        const options = document.querySelectAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand'); 
+        const hands = document.querySelectorAll('.hands img');
+        hands.forEach(hand => {
+            hand.sddEventListener ('animationend', function(){
+                this.style.animation = "";
+            });
+        });
+        const computerOptions =['rock', 'paper', 'scissors'];
+        options.forEach (option => {
+            option.addEventListener('click', function(){
+                const computerNumber = Math.floor(Math.random) * 3;
+                const computerChoice = computerOptions[computerNumber];
+                
+               setTimeout(() => { 
+                   compareHands(this.textContent, computerChoice);
+
+                playerHand.src =`./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
+               }, 2000);
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+            });
+        });
+    };
+    const updateScore = ()=>{
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
+    const compareHands = (playerChoice, computerChoice) =>{
+        const winner = document.querySelector('.winner');
+        if(playerChoice === computerChoice){
+            winner.textContent = 'It is a tie!';
+        }
+        if(playerChoice ==='rock'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        if(playerChoice ==='paper'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+        if(playerChoice ==='scissors'){
+            if(computerChoice === 'rock'){
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+    }
+    
+    
+};
+
+//game();
 var life = 100;
 var x = 1000;       //GLOBAL
     
@@ -163,3 +262,7 @@ console.log( dessert.fullName +"is my favorite" +dessert.lastName)
                                
 const meals = ["For One", "For Two", "For Family", "For Potluck"]; //ARRAY
 document.getElementById("demo").innerHTML = meals.join("*");      //ARRAY METHOD
+
+
+
+
